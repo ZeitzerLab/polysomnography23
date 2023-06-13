f964e9fddf18f7e05d56e004ce1d601b694b6c19
Dwijen Chawra <dchawra@purdue.edu>
2023-06-12 22:03:11 -0400

    reformatting the codebase for slurm compatibility

--- feat_imp_plotter.py
+++ feat_imp_plotter.py
@@ -15,64 +15,32 @@ import json
 import argparse
 
 # =================================================
-col2drop = ["LTDP10", "REST10", "ESS_s1", "nsrrid"] # these are all the response variables
+col2drop = ["LTDP10", "REST10", "ESS_s1", "nsrrid"]
 target_column = "LTDP10"
+filename = "datafullnight2_SE_waso.csv"
 # =================================================
 
 import wandb
 wandb.init(project="visualize-sklearn")
 
 argparser = argparse.ArgumentParser()
-argparser.add_argument('--input_threshold', type=int, default=2)
-argparser.add_argument('--threads', type=int, default=32)
+argparser.add_argument('--param_file', type=str)
 args = argparser.parse_args()
-input_threshold = args.input_threshold
+hypfilepath = args.param_file
 
 
-def rf_optimizer(xtrain, ytrain):
-    # Create the random grid
-    # Number of trees in random forest
-    n_estimators = [int(x) for x in np.arange(start=50, stop=1000, step=100)]
-    n_estimators = [10, 20, 50, 100, 200, 500, 1000]
-    # Number of features to consider at every split
-    max_features = ['auto', 'sqrt']
-    # Maximum number of levels in tree
-    max_depth = [int(x) for x in np.arange(2, 100, step=10)]
-    max_depth.append(None)
-    # Minimum number of samples required to split a node
-    min_samples_split = [5, 7, 8, 10, 20]
-    # Minimum number of samples required at each leaf node
-    min_samples_leaf = [2, 4, 8, 12]
-    # Create the random grid
-    random_grid = {'n_estimators': n_estimators,
-                   'max_features': max_features,
-                   'max_depth': max_depth,
-                   'min_samples_split': min_samples_split,
-                   'min_samples_leaf': min_samples_leaf}
-
-    # Use the random grid to search for best hyperparameters
-    # First create the base model to tune
-    # Random search of parameters, using 3 fold cross validation,
-    # search across 100 different combinations, and use all available cores
-    rf_regressor = RandomizedSearchCV(estimator=RandomForestRegressor(bootstrap=True), param_distributions=random_grid,
-                                      n_iter=25,
-                                      cv=5, verbose=1,
-                                      random_state=42, n_jobs=threads)
-    # Fit the random search model
-    rf_regressor.fit(xtrain, ytrain)
-    pprint(rf_regressor.best_params_)
-    return rf_regressor.best_params_
 
 finalarray = ["second interval", "rf_params", "lasso_params", "rf_r^2", "lasso_r^2", "WASO (min)"]
 
 # Importing the dataset
-
 df = pd.read_csv(filename)
 
 print(df)
 
 X = df.drop(columns=col2drop)
 y = df.iloc[:, df.columns.get_loc(target_column)].values
+
+output_data = []
 
 xtr, xtest, ytr, ytest = train_test_split(X.values, y, test_size=0.25, random_state=0)
 
@@ -117,3 +85,7 @@ plt.savefig(
 plt.show()
 
 np.savetxt("autoscored_eff_plots/output_" + str(input_threshold) + ".csv", np.asarray(output_data), delimiter=",", fmt='%s')
+
+finalarray.append(output_data)
+
+np.savetxt("autoscored_eff_plots/finaldata.csv", np.asarray(finalarray), delimiter=",", fmt='%s')

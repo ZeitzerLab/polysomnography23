62fb53f08e9b38206bfb9aa07a5ef1ca523a7e30
Dwijen Chawra <dchawra@purdue.edu>
2023-06-15 13:36:06 -0400

    added csv generation

--- runner_plotter.py
+++ runner_plotter.py
@@ -1,9 +1,23 @@
 import os
 import subprocess
-thresholds = [.25, 0.5, 0.75, 1, 2, 3, 4, 5, 10]
 
+# ==========================================================
+thresholds = [.25, 0.5, 0.75, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+threads = 128
+# ==========================================================
+
+
+print("Generating CSV Data")
+
+for i in range(len(thresholds)):
+    thresholds[i] = str(thresholds[i])
+
+subprocess.run(["python", "generate_append_WASO.py", "--thresholds"] + thresholds)
+
+
+threads = str(threads)
 for threshold in thresholds:
-    print("---------  threshold: " + str(threshold))
-    # subprocess.run(["python", "all_data_analysis.py", "--threads", "128", "--wasoint", str(threshold)])
-    subprocess.run(["python", "feat_imp_plotter.py", "--wasoint", str(threshold)])
+    print("---------  threshold: " + threshold)
+    subprocess.run(["python", "all_data_analysis.py", "--threads", threads, "--wasoint", threshold])
+    subprocess.run(["python", "feat_imp_plotter.py", "--wasoint", threshold])
 

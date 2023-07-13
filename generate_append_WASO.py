b1b1db37a2b308802104bc18e28cdd7e1579d0f6
Dwijen Chawra <dwchawra@gmail.com>
2023-07-13 11:15:03 -0700

    finshed duration calculation

--- generate_append_WASO.py
+++ generate_append_WASO.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
@@ -22,6 +21,7 @@ print("Duration included: " + str(duration))
 print("Frequency included: " + str(frequency))
 print("Columns to drop: " + str(cols2drop))
 
+
 temp = []
 for i in thresholds:
     if "." in i:
@@ -32,32 +32,36 @@ for i in thresholds:
 thresholds = temp
 print(thresholds)
 
-def calculate_waso(threshold_minutes, filepath):
+def calculate_waso_and_duration(threshold_minutes, filepath):
     with open(filepath, 'r') as file:
         lines = file.readlines()
 
-    current_zero_window_length = 0
+    current_nonzero_window_length = 0
     waso = 0
     count = 0
+    sleepduration = 0
 
     for line in lines:
         digit = int(line.strip())
 
         if digit == 0:
-            current_zero_window_length += 1
+            current_nonzero_window_length += 1
         else:
-            if current_zero_window_length > 0:
-                wake_time_min = (15 * current_zero_window_length) / 60
-                current_zero_window_length = 0
+            sleepduration += .25
+            if current_nonzero_window_length > 0:
+                wake_time_min = (15 * current_nonzero_window_length) / 60
+                current_nonzero_window_length = 0
 
                 if wake_time_min > threshold_minutes:
                     waso += wake_time_min
                     count += 1
-    return waso, count
+    return waso, count, sleepduration
 
 
 wasoresults = []
 freqresults = []
+sleepdurations = []
+
 for threshold in thresholds:
     wasoresults.append([])
     freqresults.append([])
@@ -66,38 +70,73 @@ original_csv = "csvdata/datafullnight2_SE.csv"
 directory_path = Path("donehypnogram")
 files = os.listdir(directory_path)
 files = sorted(files)
-# fileids = [filename.split('.')[0].split('-')[1] for filename in files]
+
+# not in ids: 200033
+# not in ids: 201671
+# not in ids: 202310
+# not in ids: 202708
+# not in ids: 203135
+
+# drop files not in ids
+files.remove("shhs1-200033.hypnogram.txt")
+files.remove("shhs1-201671.hypnogram.txt")
+files.remove("shhs1-202310.hypnogram.txt")
+files.remove("shhs1-202708.hypnogram.txt")
+files.remove("shhs1-203135.hypnogram.txt")
+
+fileids = [filename.split('.')[0].split('-')[1] for filename in files]
 
 df = pd.read_csv(original_csv)
+
+# DROP BAD ROWS (see notes.txt)
+df = df[df["nsrrid"] != 200230]
+df = df[df["nsrrid"] != 202180]
+df = df[df["nsrrid"] != 202310]
+df = df[df["nsrrid"] != 203065]
 
 ids = [str(i) for i in df["nsrrid"].values]
 
 # # show items in ids not in files
-# for i in ids:
-#     if i not in fileids:
-#         print(i)
+
+#ids is generated from the original csv
+#fileids is generated from the files in the directory
+for i in fileids:
+    if i not in ids:
+        print("not in ids: " + i)
+
+print(len(ids))
+print(len(fileids))
+    
 
 for filename in tqdm(files):
     # print(filename.split('.')[0].split('-')[1])
     if filename.split('.')[0].split('-')[1] in ids:
         file_path = os.path.join(directory_path, filename)
+        sleepdur = 0
         for i, threshold in enumerate(thresholds):
-            waso, freq = calculate_waso(threshold, file_path)
+            waso, freq, sleepdur = calculate_waso_and_duration(threshold, file_path)
             wasoresults[i].append(waso)
             freqresults[i].append(freq)
+        sleepdurations.append(sleepdur)
 
 # Dropping the columns
 df = df.drop(cols2drop, axis=1)
 
 # # TODO: make the sleep to wake column average over total sleep time
-# # TODO: Install numpy / pandas bc you deleted the env
 
 # # sum of ["N1 (min)", "N2 (min)", "N3 (min)", "REM (min)"] is sleep duration
 
-# # add a sleep duration column
+# # add a sleep duration column 
 
-# durationcol = [
+# add to original DF because this value is not thresholded
+import pprint
+for i in zip(files, sleepdurations):
+    pprint.pprint(i)
 
+# print last 5 of files and ids lists
+print(files[-5:])
+print(ids[-5:])
+exit()
 allcols = df.copy(deep=True)
 
 print("Generating csv with all cols")
